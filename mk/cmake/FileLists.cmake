c27023d09efde6609e0122d57e1876d3e92d2ce6
aiq <alex.schulz.mail@googlemail.com>
2022-07-06 14:44:05 +0200

    removed unused oEbmlElementStack

--- mk/cmake/FileLists.cmake
+++ mk/cmake/FileLists.cmake
@@ -24,7 +24,6 @@ set( INC_FILES
 ################################################################################
 set( SRC_FILES
    ${SRC_DIR}/_/misc.c
-   ${SRC_DIR}/_/oEbmlElementStack.c
 
    ${SRC_DIR}/oddebml/error.c
    ${SRC_DIR}/oddebml/o_EbmlType.c
--- src/_/oEbmlElementStack.c
+++
@@ -1,8 +0,0 @@
-#include "_/oEbmlElementStack.h"
-
-STACK_IMPL_C_(
-   oEbmlElement,        // Type
-   oEbmlElementStack,   // StackType
-   ebml_element_o,      // FuncSuffix
-   ebml_element_stack_o // StackFuncSuffix
-)
